5aeda94815e0ae640390c3b4afc45e110d0855ca
windini <aquariuswin1424@gmail.com>
2025-02-01 16:47:25 +0700

    first commit

---
+++ lib/src/data/models/alert_request.dart
@@ -0,0 +1,11 @@
+import 'package:math_metrix_app/src/core/app_constant.dart';
+
+class AlertRequest {
+  final GameCategoryType gameCategoryType;
+  final String type;
+  final double score;
+  final double coin;
+  final bool isPause;
+
+  AlertRequest({required this.type,required this.gameCategoryType,required this.score,required this.coin,required this.isPause});
+}
